b05612afc7edb0e7aaee35d37f4a6560f58cd1f6
yazanaka <yznwwk1@gmail.com>
2019-06-10 17:58:46 +0300

    still fix some problem

---
+++ sooq/components/userPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
+export default class UserPage extends Component {
+	render() {
+		return (
+			<View>
+				<Text>hollo iam userpage</Text>
+			</View>
+		);
+	}
+}
